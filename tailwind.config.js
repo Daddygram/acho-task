5c243fc474f264c1fc1b8d3f3aacac4b6f282a5b
Joseph Moninyo <tsotne.s.2004@gmail.com>
2024-04-19 22:55:09 +0400

    mostly finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
     extend: {
       colors: {
         blackPrimary: "#0E0D0D",
-        whitePrimary: "#FFFFFF",
+        whitePrimary: "#F5F5F5",
         beigePrimary: "#DCC6B6",
         secondary: "#F4EFEB",
         greyish: "#212121",
         lightPink:"#DADADA",
+        alert: "#F7DAD9",
+        alertText: "#B91C1C"
       },
       screens: {
         "2xl": "1440px",
